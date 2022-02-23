50d8fbf86bc488507e0128f59f4d15a0856855d9
Ivan Kondratev <80636346+IvanXablin@users.noreply.github.com>
2022-02-23 23:28:26 +0700

    add SieveOfEratosthenes

---
+++ AlgorithmDataStructures/SieveOfEratosthenes/SieveOfEratosthenes/ArrayOperation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SieveOfEratosthenes
+{
+    public class ArrayOperation
+    {
+       public void FillList(List<int> list) 
+       {
+            for (int i = 2; i < 30; i++)
+            {
+                list.Add(i);
+            }
+       }
+
+        public void PrintList(List<int> list)
+        {
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public void Eratoshen(List<int> list)
+        {
+            for(int i = 0; i < list.Count; i++)
+            {
+                for (int j = 2; j < 30; j++)
+                {
+                    list.Remove(list[i] * j);
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmDataStructures/SieveOfEratosthenes/SieveOfEratosthenes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SieveOfEratosthenes
+{
+    class Program
+    {
+       
+
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>(30);
+
+            ArrayOperation ao = new ArrayOperation();
+            ao.FillList(list);
+            ao.Eratoshen(list);
+            ao.PrintList(list);
+        }
+    }
+}
